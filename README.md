28f90d93cd2bd8accb031d1075d5966cd0abaf3c
chadless1 <chadless1@gmail.com>
2024-06-13 21:58:28 -0400

    simplified js and cleanup

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 
 # Bucket Golf Scorecard
 
+scorecard for bucketgolf
+
 track your score for 6 holes
 
 every hole is a par 3
@@ -10,3 +12,9 @@ lowest score wins!
 [View Scorecard](https://chadless1.github.io/bucketgolf_scorecard/)
 
 ![ScreenShot](screenshot.png)
+
+### TODO:
+
+- add dropdown for more course options / game modes
+- retain data on refresh to not lose scores
+- firefox NaN problem
--- js/cal_player_score.js
+++ js/cal_player_score.js
@@ -1,47 +1,35 @@
-function calScore(button) {
+function calScore() {
 	
 	// get scores of all 4 players and calulate total score
 
 	//player 1 scores
-	var p1_h1_score = document.getElementById("p1_h1s").innerHTML;
-	var p1_h2_score = document.getElementById("p1_h2s").innerHTML;
-	var p1_h3_score = document.getElementById("p1_h3s").innerHTML;
-	var p1_h4_score = document.getElementById("p1_h4s").innerHTML;
-	var p1_h5_score = document.getElementById("p1_h5s").innerHTML;
-	var p1_h6_score = document.getElementById("p1_h6s").innerHTML;
-	//total all player 1 hole scores
-	var sum_p1_total = Number(p1_h1_score) + Number(p1_h2_score) + Number(p1_h3_score) + Number(p1_h4_score) + Number(p1_h5_score) + Number(p1_h6_score);
+	const p1_scores = document.querySelectorAll(".p1_hs");
+	let sum_p1_total = Number(0);
+	for (let i=0; i < p1_scores.length; i++) {
+		sum_p1_total += Number(p1_scores[i].innerHTML);
+	}
 
 	//player 2 scores
-	var p2_h1_score = document.getElementById("p2_h1s").innerHTML;
-	var p2_h2_score = document.getElementById("p2_h2s").innerHTML;
-	var p2_h3_score = document.getElementById("p2_h3s").innerHTML;
-	var p2_h4_score = document.getElementById("p2_h4s").innerHTML;
-	var p2_h5_score = document.getElementById("p2_h5s").innerHTML;
-	var p2_h6_score = document.getElementById("p2_h6s").innerHTML;
-	//total all player 2 hole scores
-	var sum_p2_total = Number(p2_h1_score) + Number(p2_h2_score) + Number(p2_h3_score) + Number(p2_h4_score) + Number(p2_h5_score) + Number(p2_h6_score);
-
+	const p2_scores = document.querySelectorAll(".p2_hs");
+	let sum_p2_total = Number(0);
+	for (let i=0; i < p2_scores.length; i++) {
+		sum_p2_total += Number(p2_scores[i].innerHTML);
+	}
+	
 	//player 3 scores
-	var p3_h1_score = document.getElementById("p3_h1s").innerHTML;
-	var p3_h2_score = document.getElementById("p3_h2s").innerHTML;
-	var p3_h3_score = document.getElementById("p3_h3s").innerHTML;
-	var p3_h4_score = document.getElementById("p3_h4s").innerHTML;
-	var p3_h5_score = document.getElementById("p3_h5s").innerHTML;
-	var p3_h6_score = document.getElementById("p3_h6s").innerHTML;
-	//total all player 3 hole scores
-	var sum_p3_total = Number(p3_h1_score) + Number(p3_h2_score) + Number(p3_h3_score) + Number(p3_h4_score) + Number(p3_h5_score) + Number(p3_h6_score);
-
+	const p3_scores = document.querySelectorAll(".p3_hs");
+	let sum_p3_total = Number(0);
+	for (let i=0; i < p3_scores.length; i++) {
+		sum_p3_total += Number(p3_scores[i].innerHTML);
+	}
+	
 	//player 4 scores
-	var p4_h1_score = document.getElementById("p4_h1s").innerHTML;
-	var p4_h2_score = document.getElementById("p4_h2s").innerHTML;
-	var p4_h3_score = document.getElementById("p4_h3s").innerHTML;
-	var p4_h4_score = document.getElementById("p4_h4s").innerHTML;
-	var p4_h5_score = document.getElementById("p4_h5s").innerHTML;
-	var p4_h6_score = document.getElementById("p4_h6s").innerHTML;
-	//total all player 4 hole scores
-	var sum_p4_total = Number(p4_h1_score) + Number(p4_h2_score) + Number(p4_h3_score) + Number(p4_h4_score) + Number(p4_h5_score) + Number(p4_h6_score);
-
+	const p4_scores = document.querySelectorAll(".p4_hs");
+	let sum_p4_total = Number(0);
+	for (let i=0; i < p4_scores.length; i++) {
+		sum_p4_total += Number(p4_scores[i].innerHTML);
+	}
+	
 	// display player scores
 	document.getElementById("p1s").innerHTML = sum_p1_total;
 	document.getElementById("p2s").innerHTML = sum_p2_total;
